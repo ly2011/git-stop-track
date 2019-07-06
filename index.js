9106d5e5ba7b230409fb7139d954405a5c1c1780
ex_luyun1 <yun2.lu@partner.midea.com>
2019-07-06 12:54:50 +0800

    <bug>: 修复过滤正则方式的文件形式

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ const path = require('path')
 const resolve = function(dir) {
   return path.resolve(__dirname, dir)
 }
+
+const difference = (arr1, arr2) => {
+  const s = new Set(arr2)
+  return arr1.filter(x => !s.has(x))
+}
+
 function updateGitignoreConfig() {
   const gitPath = resolve('.git')
   const gitignorePath = resolve('./.gitignore')
@@ -41,10 +47,22 @@ function updateGitignoreConfig() {
     return console.error('.git 文件夹不存在')
   }
 
-  fs.appendFile(gitignorePath, gitignoreFiles.join('\n') + '\n', err => {
+  const lines = fs
+    .readFileSync(gitignorePath)
+    .toString()
+    .split('\n')
+  const needGitignoreFiles = difference(gitignoreFiles, lines)
+  const needGitignoreFilesStr =
+    needGitignoreFiles.length > 0 ? needGitignoreFiles.join('\n') + '\n' : ''
+  console.log('needGitignoreFiles: ', needGitignoreFiles)
+  if (!needGitignoreFilesStr) {
+    assumeUnchangedFiles(gitignoreFiles) // 至于为何不是传入needGitignoreFiles而是gitignoreFiles，是为了有童鞋已经手动添加到 `.gitignore` 文件导致没有实际停止追踪文件
+    return
+  }
+  fs.appendFile(gitignorePath, needGitignoreFilesStr, err => {
     if (err) return console.error(err)
     console.log('向 gitignore 追加内容成功')
-    assumeUnchangedFiles(gitignoreFiles)
+    assumeUnchangedFiles(gitignoreFiles) // 至于为何不是传入needGitignoreFiles而是gitignoreFiles，是为了有童鞋已经手动添加到 `.gitignore` 文件导致没有实际停止追踪文件
   })
 }
 
@@ -52,11 +70,11 @@ function assumeUnchangedFiles(gitignoreFiles = []) {
   const promiseFiles = gitignoreFiles.map(
     file =>
       new Promise((resolve, reject) => {
-        if (!fs.existsSync(path.resolve(file))) {
-          return resolve()
-        }
+        // if (!fs.existsSync(path.resolve(file))) {
+        //   return resolve()
+        // }
         exec(`git update-index --assume-unchanged ${file}`, err => {
-          console.log('我被执行了: ', file)
+          // console.log('我被执行了: ', file)
           // if (err) reject(err)
           resolve()
         })
